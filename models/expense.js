4e7ebff7eda0a33419526c992a5cb55695ae26b9
Shamseera <kurumbeths@gmail.com>
2020-07-10 16:20:41 +1000

    added expense model

---
+++ models/expense.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+const Expense = new Schema({
+    item: {
+        type: String,
+        required: [true, 'Item is required']
+    },
+    amount: {
+        type: Number,
+        required: [true, 'Amount is required']
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    category: {
+        type: String,
+        required: [true, 'category is required']
+    } ,
+    notes: {
+        type: String,
+    } ,
+
+})
+
+module.exports = mongoose.model('Expense', Expense)
